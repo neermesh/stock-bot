f36dbcd9a1d6d3fba841073847c5ad019c3d1a7f
Neeraj Meshram <neermesh7861@gmail.com>
2023-09-27 20:21:06 +0530

    fyers api auth code and historic data

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+- first run generate_auth_code.py
+- it will generate one link
+- click on that link
+- copy auth code and paste it in auth_code file
+- then run generate_token.py
+- it will genrate token
+- copy this token and paste it in access_token file
+
+'''
